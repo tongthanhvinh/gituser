e0549a3473cb47187190595eb2cf648c8c6ec119
Vinh Tong <vjnhgom@gmail.com>
2025-02-27 19:49:45 +0700

    Initial Commit

---
+++ TymeTests/TymeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TymeTests.swift
+//  TymeTests
+//
+//  Created by Vinh Tong on 27/2/25.
+//
+
+import Testing
+@testable import Tyme
+
+struct TymeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
