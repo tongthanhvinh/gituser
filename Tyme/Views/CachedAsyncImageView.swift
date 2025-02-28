ee7a19dda1a9eafdba99c93567f4b5273d9200b3
Vinh Tong <vjnhgom@gmail.com>
2025-02-28 19:15:15 +0700

    Optimize Async Image

--- Tyme/Views/CachedAsyncImageView.swift
+++ Tyme/Views/CachedAsyncImageView.swift
@@ -24,50 +24,14 @@ struct CachedAsyncImageView: View {
                     .renderingMode(.template)
                     .foregroundStyle(Color.gray.opacity(0.5))
                     .padding()
-                    .onAppear(perform: loadImage)
+                    .task {
+                        image = await ImageCache.shared.loadImage(urlStr: urlStr)
+                    }
             }
         }
     }
-
-    private func loadImage() {
-        guard let urlStr = urlStr else {
-            return
-        }
-        if let cachedImage = ImageCache.shared.get(forKey: urlStr) {
-            self.image = cachedImage
-        } else {
-            downloadImage()
-        }
-    }
-
-    private func downloadImage() {
-        guard let urlStr = urlStr, let url = URL(string: urlStr) else {
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data, let uiImage = UIImage(data: data) {
-                ImageCache.shared.set(uiImage, forKey: url.absoluteString)
-                DispatchQueue.main.async {
-                    self.image = uiImage
-                }
-            }
-        }
-        .resume()
-    }
 }
 
-class ImageCache {
-    static let shared = ImageCache()
-    private let cache = NSCache<NSString, UIImage>()
-
-    func get(forKey key: String) -> UIImage? {
-        return cache.object(forKey: key as NSString)
-    }
-
-    func set(_ image: UIImage, forKey key: String) {
-        cache.setObject(image, forKey: key as NSString)
-    }
-}
 
 #Preview {
     CachedAsyncImageView(urlStr: nil)
